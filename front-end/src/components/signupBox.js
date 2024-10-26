6a7cbd33eb98ec692b6cfbe68c44ce714347836e
vamshiboju <83817973+vamshiboju@users.noreply.github.com>
2024-10-26 07:21:16 -0500

    Update signupBox.js

--- front-end/src/components/signupBox.js
+++ front-end/src/components/signupBox.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import LocationSvg from "../media/LocationSvg.svg";
 import Loading from "./loading";
 
-const ENDPOINT = "http://localhost:4000";
+const ENDPOINT = "https://connectingworld-backend.onrender.com";
 
 
 const SignupBox = (props) => {
